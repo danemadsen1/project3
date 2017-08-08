35a6491c173c7640d1ed123b19e8c915fae6f3f6
Dane Madsen <dmadsen@mitre.org>
2017-08-08 14:18:20 -0400

    Added second line to program output

--- hello.java
+++ hello.java
@@ -1,7 +1,10 @@
 // Hello world
+// Updated for Project3 on 8/8/17 at 2:17pm
+//
 class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
+		System.out.println("This is Project3!");
 	}
 }
 

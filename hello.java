55654dbbfd14fa0a97ea0e3ef26abfc37677913b
Dane Madsen <dmadsen@mitre.org>
2017-08-08 14:20:54 -0400

    Added a third output line

--- hello.java
+++ hello.java
@@ -1,10 +1,11 @@
 // Hello world
-// Updated for Project3 on 8/8/17 at 2:17pm
+// Updated for Project3 on 8/8/17 at 2:20pm
 //
 class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello world!");
 		System.out.println("This is Project3!");
+		System.out.println("We keep making more changes!");
 	}
 }
 
